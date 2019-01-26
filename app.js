7d7c7d0760e817f01108dff5afb1db8f44beb344
Seth Deegan <jayandseth@gmail.com>
2019-01-26 11:28:33 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.use(express.static('./pages'))
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
