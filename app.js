10173b6384f39251e51a8d2433ad42887589308a
Seth Deegan <jayandseth@gmail.com>
2019-01-26 16:45:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT
-
-app.use(express.static('./pages'))
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+var React = require('react');
+var ReactDOM = require('react-dom');
+ 
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+ 
+ReactDOM.render(<MyComponent />, node);
