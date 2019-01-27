9ab5c1ffb1dce728888195764fdffb3489d69372
Seth Deegan <jayandseth@gmail.com>
2019-01-27 13:59:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+console.log('Something worked');
+
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root')
