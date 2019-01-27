6567410e320ec6e3c93e125559793f88f633db1b
Seth Deegan <jayandseth@gmail.com>
2019-01-27 13:47:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const port = process.env.PORT
 var React = require('react');
 var ReactDOM = require('react-dom');
  
-class MyComponent extends React.Component {
-  render() {
-    return <div>Hello World</div>;
-  }
-}
- 
-ReactDOM.render(<MyComponent />, node);
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
