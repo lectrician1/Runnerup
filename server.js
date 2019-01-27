cceae6b6c61eca31b1a44c4845c6acba3f03bd89
Seth Deegan <jayandseth@gmail.com>
2019-01-27 14:25:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
  
