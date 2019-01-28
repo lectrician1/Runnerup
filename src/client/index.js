3acd2de4c7816a0bd8e45b1c278239bfa8981a2a
Seth Deegan <jayandseth@gmail.com>
2019-01-27 21:17:12 -0600

    Update and rename index.js to src/client/index.js

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import { Text, View } from 'react-native';
+
+ReactDOM.render({
+    <View>
+        <Text>Hello world!</Text>
+    </View>
+  },
+  document.getElementById('root')
+);
