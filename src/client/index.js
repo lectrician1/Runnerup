a5ce8f8bb2fee5563d1183050262cee860229d6a
Seth Deegan <jayandseth@gmail.com>
2019-01-27 21:44:07 -0600

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
  ReactDOM.render(
-  <h1>Welcome to Runnerup! NCHS's XCTF App</h1>,
+  <h1>Welcome to Runnerup! NCHS\'s XCTF App</h1>,
   document.getElementById('root')
 );
